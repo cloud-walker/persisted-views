c62d3580885abe4b9924e533d943edd19f5b2dda
Luca Barone <baro.luc@gmail.com>
2026-07-11 16:08:39 +0200

    play with zustand

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,23 @@
 import { getViews } from "#/services/view";
 import { createFileRoute } from "@tanstack/react-router";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+type HomeViewState = { name: string };
+const useViewStore = create(
+  persist<{ state: HomeViewState; setState: (state: HomeViewState) => void }>(
+    (set) => ({
+      state: {
+        name: "",
+      },
+      setState: (state: { name: string }) => set({ state }),
+    }),
+    {
+      name: "home",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
 
 export const Route = createFileRoute("/")({
   loader: () => {
@@ -9,14 +27,21 @@ export const Route = createFileRoute("/")({
 });
 
 function Home() {
-  const views = Route.useLoaderData();
+  const state = useViewStore((s) => s.state);
+  const setState = useViewStore((s) => s.setState);
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
-      <pre>{JSON.stringify(views, null, 2)}</pre>
+      <input
+        type="text"
+        value={state.name}
+        onChange={(e) => {
+          setState({ name: e.target.value });
+        }}
+      />
     </div>
   );
 }
